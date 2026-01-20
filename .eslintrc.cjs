ab16859159bb9607f2e694974121ad3e56f7cb8b
Sharon Clark <sclark@ucar.edu>
2026-01-20 12:12:19 -0700

    update to stable react 18, adjust linting warnings/errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,37 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
   ],
+
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint"],
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+      extends: ["plugin:@typescript-eslint/recommended"],
+    },
+    {
+      files: ["src/pages/HomeScreen.tsx"],
+      rules: {
+        "@typescript-eslint/no-unused-vars": "warn",
+        "react-hooks/set-state-in-effect": "warn", // or "off"
+      },
+    },
+    {
+      files: [
+        "src/pages/Simulation.jsx",
+        "src/pages/EachAlone.jsx",
+        "src/pages/AllTogether.jsx",
+      ],
+      rules: {
+        "no-unused-vars": "warn",
+      },
+    },
+  ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
--- src/index.jsx
+++ src/index.jsx
@@ -3,5 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+root.render(
+  <React.StrictMode>
+    <App tab="home" />
+  </React.StrictMode>,
+);
 
